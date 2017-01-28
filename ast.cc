6f096678976bec82a2eb868f0d2c506c8263da02
Sandhya Chandramohan <sandhya2392@gmail.com>
2017-01-28 14:56:56 +0530

    ast almost completed

---
+++ ast.cc
@@ -0,0 +1,380 @@
+#include<iostream>
+#include<fstream>
+#include<typeinfo>
+
+using namespace std;
+
+#include"common-classes.hh"
+#include"error-display.hh"
+#include"user-options.hh"
+#include"local-environment.hh"
+#include"symbol-table.hh"
+#include"ast.hh"
+#include"sequence-ast.hh"
+#include"procedure.hh"
+#include"program.hh"
+
+Ast::Ast()
+{}
+
+Ast::~Ast()
+{}
+
+bool Ast::check_ast()
+{
+	stringstream msg;
+	msg << "No check_ast() function for " << typeid(*this).name();
+	CHECK_INVARIANT(CONTROL_SHOULD_NOT_REACH, msg.str());
+}
+
+Data_Type Ast::get_data_type()
+{
+	stringstream msg;
+	msg << "No get_data_type() function for " << typeid(*this).name();
+	CHECK_INVARIANT(CONTROL_SHOULD_NOT_REACH, msg.str());
+}
+
+Symbol_Table_Entry & Ast::get_symbol_entry()
+{
+	stringstream msg;
+	msg << "No get_symbol_entry() function for " << typeid(*this).name();
+	CHECK_INVARIANT(CONTROL_SHOULD_NOT_REACH, msg.str());
+}
+
+bool Ast::is_value_zero()
+{
+	stringstream msg;
+	msg << "No is_value_zero() function for " << typeid(*this).name();
+	CHECK_INVARIANT(CONTROL_SHOULD_NOT_REACH, msg.str());
+}
+
+void Ast::set_data_type(Data_Type dt)
+{
+	stringstream msg;
+	msg << "No set_data_type() function for " << typeid(*this).name();
+	CHECK_INVARIANT(CONTROL_SHOULD_NOT_REACH, msg.str());
+}
+
+////////////////////////////////////////////////////////////////
+
+Assignment_Ast::Assignment_Ast(Ast * temp_lhs, Ast * temp_rhs, int line)
+{
+	//ADD CODE HERE
+	lhs = temp_lhs;
+	rhs = temp_rhs;
+	lineno = line;
+}
+
+Assignment_Ast::~Assignment_Ast()
+{
+	//ADD CODE HERE
+	delete(lhs);
+	delete(rhs);
+}
+
+bool Assignment_Ast::check_ast()
+{
+	CHECK_INVARIANT((rhs != NULL), "Rhs of Assignment_Ast cannot be null");
+	CHECK_INVARIANT((lhs != NULL), "Lhs of Assignment_Ast cannot be null");
+
+	// use typeid(), get_data_type()
+	//ADD CODE HERE
+	if(lhs->get_data_type()!=rhs->get_data_type()){ //TODO: if lhs is ass_ast or arith_ast or num then show error
+		if(typeid(*rhs)==typeid(Number_Ast<int>) || typeid(*rhs)==typeid(Number_Ast<float>)){	//TODO: so that the same error is show
+			if(rhs->is_value_zero()){
+				return true;
+			}	
+		}	
+		CHECK_INPUT(CONTROL_SHOULD_NOT_REACH,"Assignment statement data type not compatible", lineno);
+		return false;
+	}
+	return true;
+}
+
+void Assignment_Ast::print(ostream & file_buffer)
+{
+	//ADD CODE HERE
+	file_buffer<<"\nAsign:\n\tLHS (";
+	lhs->print(file_buffer);
+	file_buffer<<")\n\tRHS (";	
+	rhs->print(file_buffer);
+	file_buffer<<")";
+}
+
+/////////////////////////////////////////////////////////////////
+
+Name_Ast::Name_Ast(string & name, Symbol_Table_Entry & var_entry, int line)
+{
+
+	CHECK_INVARIANT((variable_symbol_entry->get_variable_name() == name),
+		"Variable's symbol entry is not matching its name");
+	//ADD CODE HERE
+	variable_symbol_entry = &var_entry;
+	node_data_type = var_entry.get_data_type();
+	lineno = line;
+}
+
+Name_Ast::~Name_Ast()
+{}
+
+Data_Type Name_Ast::get_data_type()
+{
+	// refer to functions for Symbol_Table_Entry 
+	//ADD CODE HERE
+	//TODO:	CHECK_INVARIANT(node_data_type == variable_symbol_entry->get_data_type(),"Data types dont match");
+	return node_data_type;
+}
+
+Symbol_Table_Entry & Name_Ast::get_symbol_entry()
+{
+	//ADD CODE HERE
+	return *variable_symbol_entry;
+}
+
+void Name_Ast::set_data_type(Data_Type dt)
+{
+	//ADD CODE HERE
+	node_data_type = dt;
+}
+
+void Name_Ast::print(ostream & file_buffer)
+{
+	//ADD CODE HERE
+	file_buffer<<"Name : "<<variable_symbol_entry->get_variable_name();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+template <class DATA_TYPE>
+Number_Ast<DATA_TYPE>::Number_Ast(DATA_TYPE number, Data_Type constant_data_type, int line)
+{
+	// use Ast_arity from ast.hh
+	//ADD CODE HERE
+	constant = number;
+	node_data_type = constant_data_type;
+	ast_num_child = zero_arity;	//TODO
+	lineno = line;
+}
+
+template <class DATA_TYPE>
+Number_Ast<DATA_TYPE>::~Number_Ast()
+{}
+
+template <class DATA_TYPE>
+Data_Type Number_Ast<DATA_TYPE>::get_data_type()
+{
+	//ADD CODE HERE
+	return node_data_type;
+}
+
+template <class DATA_TYPE>
+void Number_Ast<DATA_TYPE>::set_data_type(Data_Type dt)
+{
+	//ADD CODE HERE
+	node_data_type = dt;
+}
+
+template <class DATA_TYPE>
+bool Number_Ast<DATA_TYPE>::is_value_zero()
+{
+	//ADD CODE HERE
+	if(constant==0)
+		return true;
+	else 
+		return false;	
+}
+
+template <class DATA_TYPE>
+void Number_Ast<DATA_TYPE>::print(ostream & file_buffer)
+{
+	//ADD CODE HERE
+	file_buffer<<"Num : "<<constant;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+Data_Type Arithmetic_Expr_Ast::get_data_type()
+{
+	//ADD CODE HERE
+	return node_data_type;
+}
+
+void Arithmetic_Expr_Ast::set_data_type(Data_Type dt)
+{
+	//ADD CODE HERE
+	node_data_type = dt;
+}
+
+bool Arithmetic_Expr_Ast::check_ast()
+{
+	// use get_data_type(), typeid()
+	//ADD CODE HERE	
+	if(lhs->get_data_type()!=rhs->get_data_type()){ //TODO:
+		if(typeid(*rhs)==typeid(Number_Ast<int>) || typeid(*rhs)==typeid(Number_Ast<double>)){	//TODO:
+			if(rhs->is_value_zero()){
+				return true;
+			}	
+		}	
+		if(typeid(*lhs)==typeid(Number_Ast<int>) || typeid(*lhs)==typeid(Number_Ast<double>)){	//TODO:
+			if(lhs->is_value_zero()){
+				return true;
+			}	
+		}	
+		CHECK_INPUT(CONTROL_SHOULD_NOT_REACH, "Arithmetic statement data type not compatible", lineno);
+		return false;
+	}
+	return true;	
+}
+
+/////////////////////////////////////////////////////////////////////
+
+Plus_Ast::Plus_Ast(Ast * l, Ast * r, int line)
+{
+	// set arity and data type
+	//ADD CODE HERE
+	lhs = l;
+	rhs = r;
+	ast_num_child = binary_arity;
+	if(typeid(*rhs)==typeid(number_ast<int>) || typeid(*rhs)==typeid(number_ast<double>)){	
+		if(rhs->is_value_zero()){
+			node_data_type = l->get_data_type(); 		
+		}
+	else(typeid(*lhs)==typeid(number_ast<int>) || typeid(*lhs)==typeid(number_ast<double>)){	
+		if(lhs->is_value_zero()){
+			node_data_type = r->get_data_type(); 
+		}	
+	}
+	else 
+		node_data_type = l->get_data_type();	//todo	
+	lineno = line;
+}
+
+void plus_ast::print(ostream & file_buffer)
+{
+	//add code here
+	file_buffer<<"\narith: plus\n\tlhs (";
+	lhs->print(file_buffer);
+	file_buffer<<")\nrhs (";
+	rhs->print(file_buffer);
+	file_buffer<<")";
+}
+
+/////////////////////////////////////////////////////////////////
+
+minus_ast::minus_ast(ast * l, ast * r, int line)
+{
+	//add code here
+	lhs = l;
+	rhs = r;
+	ast_num_child = binary_arity;
+	if(typeid(*rhs)==typeid(number_ast<int>) || typeid(*rhs)==typeid(number_ast<double>)){	
+		if(rhs->is_value_zero()){
+			node_data_type = l->get_data_type(); 		
+		}
+	else(typeid(*lhs)==typeid(number_ast<int>) || typeid(*lhs)==typeid(number_ast<double>)){	
+		if(lhs->is_value_zero()){
+			node_data_type = r->get_data_type(); 
+		}	
+	}
+	else 
+		node_data_type = l->get_data_type();	//todo	
+	lineno = line;
+}
+
+void minus_ast::print(ostream & file_buffer)
+{
+	//add code here
+	file_buffer<<"\narith: minus\n\tlhs (";
+	lhs->print(file_buffer);
+	file_buffer<<")\nrhs (";
+	rhs->print(file_buffer);
+	file_buffer<<")";
+}
+
+//////////////////////////////////////////////////////////////////
+
+mult_ast::mult_ast(ast * l, ast * r, int line)
+{
+	//add code here
+	lhs = l;
+	rhs = r;
+	ast_num_child = binary_arity;
+	if(typeid(*rhs)==typeid(number_ast<int>) || typeid(*rhs)==typeid(number_ast<double>)){	
+		if(rhs->is_value_zero()){
+			node_data_type = l->get_data_type(); 		
+		}
+	else(typeid(*lhs)==typeid(number_ast<int>) || typeid(*lhs)==typeid(number_ast<double>)){	
+		if(lhs->is_value_zero()){
+			node_data_type = r->get_data_type(); 
+		}	
+	}
+	else 
+		node_data_type = l->get_data_type();	//todo	
+	lineno = line;
+}
+
+void mult_ast::print(ostream & file_buffer)
+{
+	//ADD CODE HERE
+	file_buffer<<"\nArith: MULT\n\tLHS (";
+	lhs->print(file_buffer);
+	file_buffer<<")\nRHS (";
+	rhs->print(file_buffer);
+	file_buffer<<")";
+}
+
+////////////////////////////////////////////////////////////////////
+
+Divide_Ast::Divide_Ast(Ast * l, Ast * r, int line)
+{
+	//ADD CODE HERE
+	lhs = l;
+	rhs = r;
+	ast_num_child = binary_arity;
+	if(typeid(*rhs)==typeid(number_ast<int>) || typeid(*rhs)==typeid(number_ast<double>)){	
+		if(rhs->is_value_zero()){
+			node_data_type = l->get_data_type(); 		
+		}
+	else(typeid(*lhs)==typeid(number_ast<int>) || typeid(*lhs)==typeid(number_ast<double>)){	
+		if(lhs->is_value_zero()){
+			node_data_type = r->get_data_type(); 
+		}	
+	}
+	else 
+		node_data_type = l->get_data_type();	//todo	
+	lineno = line;
+}
+
+void Divide_Ast::print(ostream & file_buffer)
+{
+	//ADD CODE HERE
+	file_buffer<<"\nArith: DIV\n\tLHS (";
+	lhs->print(file_buffer);
+	file_buffer<<")\nRHS (";
+	rhs->print(file_buffer);
+	file_buffer<<")";
+}
+
+//////////////////////////////////////////////////////////////////////
+
+UMinus_Ast::UMinus_Ast(Ast * l, Ast * r, int line)
+{
+	//ADD CODE HERE
+	lhs = l;
+	rhs = r;
+	ast_num_child = unary_arity;
+	node_data_type = l->get_data_type();	//todo	
+	lineno = line;
+}
+
+void UMinus_Ast::print(ostream & file_buffer)
+{
+	//ADD CODE HERE
+	file_buffer<<"\nArith: UMNIUS\n\tLHS (";
+	lhs->print(file_buffer);
+	file_buffer<<")";
+}
+
+
+template class Number_Ast<double>;
+template class Number_Ast<int>;
---
+++ scanner.ll
@@ -0,0 +1,95 @@
+%filenames="scanner"
+%lex-source="scanner.cc"
+
+digit [0-9]
+alphabet [a-zA-Z_]
+//number([-+]{digit}+)|({digit}+)
+number {digit}+
+identifier {alphabet}({alphabet}|{digit})*
+fl (((({number})\.({number}))|(\.{number})|({number}\.))(([eE][+-]?{number})?))|({number}[eE][+-]?{number}) 
+
+
+%%
+//ADD YOUR CODE HERE
+
+
+"int"	{
+		printf("int found\n");
+		store_token_name("INTEGER");
+//		return Parser::INTEGER;
+	}
+
+"float"	{
+		printf("float found\n");
+		store_token_name("FLOAT");
+//		return Parser::FLOAT;
+	}
+
+
+"="	{
+		printf("equal to found\n");
+		store_token_name("ASSIGN_OP");
+//		return Parser::ASSIGN;
+	}
+
+"void"	{
+		printf("void found\n");
+		store_token_name("VOID");
+//		return Parser::VOID;	
+	}
+
+"return" {
+		printf("return found\n");
+		store_token_name("RETURN");
+//		return Parser::RETURN;
+	 }
+			
+{identifier} {
+		std::cout<<"identifier found "<<matched()<<endl;
+		ParserBase::STYPE__ *val = getSval();      
+		std::string * matched_str = new string(matched()); 	
+		val -> string_value = matched_str;
+		store_token_name("NAME");
+//		return Parser::NAME; 
+             }
+
+{fl} 	{
+		std::cout<<"float value found "<<matched()<<endl;
+		ParserBase::STYPE__ *val = getSval();      
+		val -> float_value = atof(matched().c_str());
+		store_token_name("DOUBLE_NUMBER");
+//		return Parser::DOUBLE_NUMBER; 
+
+	}
+
+{number} {	
+		std::cout<<"number found "<<matched()<<endl;
+		ParserBase::STYPE__ *val = getSval();      
+	        val -> integer_value = atoi(matched().c_str());
+		store_token_name("NUM");
+//		return Parser::INTEGER_NUMBER; 
+   	}
+
+[-+*/\(\);{}] {
+		std::cout<<"other found "<<matched()<<endl;	
+		store_token_name("META CHAR");
+//		return matched()[0];
+	 }
+
+\n    		|
+";;".*  	|
+[ \t]*";;".*	|
+[ \t]*"//".*	|
+[ \t]		{
+			if (command_options.is_show_tokens_selected())
+				ignore_token();
+		}
+
+.		{ 
+			string error_message;
+			error_message =  "Illegal character `" + matched();
+			error_message += "' on line " + lineNr();
+			
+			CHECK_INPUT(CONTROL_SHOULD_NOT_REACH, error_message, lineNr());
+		}
+
